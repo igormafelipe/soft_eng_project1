f98e6a36738af2702e51f6318d9b627acf2d3f0e
Igor Mafra Felipe <imafra01@vm-hw05.eecs.tufts.edu>
2021-09-27 08:59:56 -0400

    hm

---
+++ EdgeGraphAdapter.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+public class EdgeGraphAdapter implements EdgeGraph {
+
+    private Graph g;
+
+    EdgeGraphAdapter(Graph g) { this.g = g; }
+
+    public boolean addEdge(Edge e) {
+	    if (this.g.hasEdge(e.getSrc(), e.getDst())) {
+        return false;
+      }
+      this.g.addNode(e.getSrc());
+      this.g.addNode(e.getDst());
+
+      this.g.addEdge(e.getSrc(), e.getDst());
+      return true;
+    }
+
+    public boolean hasNode(String n) {
+	    return g.hasNode(n);
+    }
+
+    public boolean hasEdge(Edge e) {
+      return g.hasEdge(e.getSrc(), e.getDst());
+    }
+
+    public boolean removeEdge(Edge e) {
+      if (!g.hasEdge(e.getSrc(), e.getDst())) { return false; }
+      g.removeEdge(e.getSrc(), e.getDst());
+      if (g.succ(e.getSrc()).size() == 0) { g.removeNode(e.getSrc());}
+      if (g.succ(e.getDst()).size() == 0) { g.removeNode(e.getDst());}
+      return true;
+    }
+
+    public List<Edge> outEdges(String n) {
+      ArrayList<Edge> toRet = new ArrayList<Edge>();
+      if (!g.hasNode(n)) { return toRet; }
+      for (String dest : g.succ(n)) {
+        toRet.add(new Edge(n, dest));
+      }
+      return toRet;
+    }
+
+    public List<Edge> inEdges(String n) {
+      ArrayList<Edge> toRet = new ArrayList<Edge>();
+      if (!g.hasNode(n)) { return toRet; }
+      for (String src : g.pred(n)) {
+        toRet.add(new Edge(src, n));
+      }
+      return toRet;
+    }
+
+    public List<Edge> edges() {
+      ArrayList<Edge> toRet = new ArrayList<Edge>();
+      for (String src : g.nodes()) {
+        for (String dest : g.succ(src)) {
+          toRet.add(new Edge(src, dest));
+        }
+      }
+      return toRet;
+    }
+
+    // EdgeGraph does not have a nodes funct, have to do it manually???
+    // How to create a new edge graph? It complains about no initializator???
+    // How to get 
+    public EdgeGraph union(EdgeGraph g) {
+      EdgeGraph toRet = new EdgeGraphAdapter(new ListGraph());
+
+      for (Edge edge : g.edges()) {
+        toRet.addEdge(edge);
+      }
+      for (Edge edge : this.edges()) {
+        toRet.addEdge(edge);
+      }
+      return toRet;
+    }
+
+    public boolean hasPath(List<Edge> e) {
+
+      System.out.println("hasPath\n");
+      // Forst check if the edge list sent makes sense, that is, if the 
+      // source of each is the dest of the next. IF not, raise exception.
+
+      for (int i = 0; i < e.size()-1; i++) {
+        if(e.get(i).getDst() != e.get(i+1).getSrc()) { throw new BadPath(); } 
+      }
+
+      // Then check if path is on the graph (what i did bellow.)
+      // If so, return true, otherwise false.
+
+      Queue<String> q = new LinkedList<String>();
+        q.add(e.get(0).getSrc());
+        for (Edge edge : e) {
+          q.add(edge.getDst());
+        }
+        String src = q.poll();
+        while(!q.isEmpty()) {
+          System.out.println(src + "->" + q.peek());
+          if (this.g.hasEdge(src, q.peek())) {
+            src = q.poll();
+          } else { return false; }
+        }
+        return true;
+    }
+}
---
+++ ListGraph_save.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+public class ListGraph implements Graph {
+    private HashMap<String, LinkedList<String>> nodes = new HashMap<>();
+
+    // Does put override the node?
+    public boolean addNode(String n) {
+        if (nodes.containsKey(n)) { return false; }
+        nodes.put(n, new LinkedList<String>());
+        return true;
+    }
+
+    public boolean addEdge(String n1, String n2) {
+        if ((nodes.containsKey(n1) == false || nodes.containsKey(n2) == false)) {
+            throw new NoSuchElementException();
+        }
+         for (String n : nodes.get(n1)) { 
+            if (n == n2) {
+                return false;
+            }
+        }
+        LinkedList<String> temp = nodes.get(n1);
+        temp.add(n2);
+        nodes.put(n1, temp);
+        return true;
+    }
+
+    public boolean hasNode(String n) {
+	     return nodes.containsKey(n);
+    }
+
+    public boolean hasEdge(String n1, String n2) {
+        for (String n : this.nodes()) { 
+            if (n == n2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeNode(String n) {
+        if (!nodes.containsKey(n)) { return false; }
+        for (String s : nodes.keySet()) {
+            LinkedList<String> temp = nodes.get(s);
+            temp.remove(n);
+            nodes.put(s, temp);
+        }
+        nodes.remove(n);
+        return true;
+    }
+
+    public boolean removeEdge(String n1, String n2) {
+        if ((nodes.containsKey(n1) == false || nodes.containsKey(n2) == false)) {
+            throw new NoSuchElementException();
+        }
+        LinkedList<String> temp = nodes.get(n1);
+        int temp_size = temp.size();
+        temp.removeFirstOccurrence(n2);
+        if (temp.size() == temp_size-1) {
+            nodes.put(n1, temp);
+            return true;
+        }
+        return false;
+    }
+
+    public List<String> nodes() {
+	    List<String> toRet = new ArrayList<String>();
+        HashSet<String> hset = new HashSet<String>(nodes.keySet());
+        Iterator<String> it = hset.iterator();
+
+        while(it.hasNext()) {
+            toRet.add(it.next());
+        }
+        return toRet;
+    }
+
+    public List<String> succ(String n) {
+        if (!nodes.containsKey(n)) { throw new NoSuchElementException(); }
+        return nodes.get(n);
+    }
+
+    public List<String> pred(String n) {
+        if (!nodes.containsKey(n)) { throw new NoSuchElementException(); }
+        List<String> edgeList = new ArrayList<String>();
+        for (String s : nodes.keySet()) {
+            if (nodes.get(s).contains(n)) {
+                edgeList.add(s);
+            }
+        }
+        return edgeList;
+    }
+
+    public Graph union(Graph g) {
+        // throw new UnsupportedOperationException();
+        Graph toRet = new ListGraph();
+        for (String node : this.nodes()) {
+            toRet.addNode(node);
+            for (String edge : this.succ(node)) {
+                if (!toRet.hasNode(edge)){
+                    toRet.addNode(edge);
+                }
+                toRet.addEdge(node, edge);
+            }
+        }
+        for(String s : g.nodes()) {
+            if (!toRet.hasNode(s)) {
+                toRet.addNode(s);
+            }
+            for (String edge : g.succ(s)) {
+                if (!toRet.hasNode(edge)){
+                    toRet.addNode(edge);
+                }
+                toRet.addEdge(s, edge);
+            }
+        }
+        return toRet;
+    }
+
+    public Graph subGraph(Set<String> nodes) {
+        Graph toRet = new ListGraph();
+        for(String s : nodes) {
+            if (this.hasNode(s)) { toRet.addNode(s); }
+        }
+        for(String s : toRet.nodes()) {
+            for (String edge : this.succ (s)) {
+                if (toRet.hasNode(edge)) { toRet.addEdge(s, edge); }   
+            }
+        }
+        return toRet;
+    }
+
+    public boolean connected(String n1, String n2) {
+        if ((nodes.containsKey(n1) == false || nodes.containsKey(n2) == false)) {
+            throw new NoSuchElementException();
+        }
+        Queue<String> q = new LinkedList<>();
+        q.add(n1);
+        String temp;
+        while(!q.isEmpty()) {
+            for (String edge : this.succ(q.poll())) {
+                if (edge == n2) { return true; }
+                q.add(edge);
+            }
+        }
+        return false;
+    }
+}
